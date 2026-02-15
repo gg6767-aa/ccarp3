ff14e935d5a5e7270c4a680aa4c69804ad15bfe7
gg6767-aa <rebootms6666@gmail.com>
2026-02-15 07:03:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "Yaaaaaay!";
 
     catImg.src = "cat_dance.gif";
 
